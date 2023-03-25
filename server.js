35a1890be5323fd263aef7c788264792ec6917ea
DeepakisDemigod <deepakthapa2.668@gmail.com>
2023-03-25 19:00:48 +0530

    added docs

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'))
 app.use(express.json());
+
+app.get('/home', (req, res) => {
+  res.render('index')
+})
 
 let todoItems = []
 
